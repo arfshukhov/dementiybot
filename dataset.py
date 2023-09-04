f20839c3875b19e68c24df42d042002948950bd2
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2023-09-04 17:10:22 +0500

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,15 @@
+API_TOKEN = '' #здесь API-KEY бота
+
+developer = 1053410847
+
+database = "dde49h0d083bkt"
+
+host = "ec2-54-155-110-181.eu-west-1.compute.amazonaws.com"
+
+#host = "127.0.0.1"
+
+user = "ygpsuremxatjtj"
+
+port = 5433
+
+password = "02233cc49e95de4dc94475ba9fdb491e8802d1438479a2a0b44db8f1d9da6b66"
